c2b05897110f119daac94dc43c796a18eb632de0
beccapham <54368313+beccapham@users.noreply.github.com>
2019-08-21 11:46:30 -0400

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,15 @@
+public class Hello {
+	public static void printHeading () {
+	System.out.println("Rebecca Pham");
+	System.out.println("Lab 1");
+	System.out.println("Introduction to JAVA");
+	System.out.println("August 21, 2019");
+	System.out.println("CMSC 151 and Section 4");
+	System.out.println();
+	}
+
+	public static void main(String [] args) {
+	printHeading();
+	System.out.println("Hello");
+	}
+}
